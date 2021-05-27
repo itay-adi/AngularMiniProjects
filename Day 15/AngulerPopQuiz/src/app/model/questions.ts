1fab5301010817b40619fd2dac3ca021a34fb851
itay-adi <itayzxcv@gmail.com>
2021-05-27 17:20:47 +0300

    pop-quiz

---
+++ Day 15/AngulerPopQuiz/src/app/model/questions.ts
@@ -0,0 +1,28 @@
+import { Question } from "./question";
+
+export const QUESTIONS: Question[] = [
+    {
+        caption: '1+1',
+        answers: ['1','2','3','4'],
+        correctAnswer: 1,
+        userAnswer: -1
+    },
+    {
+        caption: '1+2',
+        answers: ['1','2','3','4'],
+        correctAnswer: 2,
+        userAnswer: -1
+    },
+    {
+        caption: '1+3',
+        answers: ['1','2','3','4'],
+        correctAnswer: 3,
+        userAnswer: -1
+    },
+    {
+        caption: '1+4',
+        answers: ['5','2','3','4'],
+        correctAnswer: 0,
+        userAnswer: -1
+    }
+]
